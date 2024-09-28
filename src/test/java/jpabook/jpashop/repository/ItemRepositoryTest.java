c7096777e3b81516dc16290820bc0329e22736b6
yongjinpark <yjp9603@gmail.com>
2024-09-29 02:35:39 +0900

    Feat: Order controller 생성

--- src/test/java/jpabook/jpashop/repository/ItemRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/ItemRepositoryTest.java
@@ -70,6 +70,6 @@ class ItemRepositoryTest {
     void 재고_업데이트(Item itemParam) { //itemParam: 파리미터로 넘어온 준영속 상태의 엔티티
         Item findItem = em.find(Item.class, itemParam.getId()); //같은 엔티티를 조회한다.
         findItem.setPrice(itemParam.getPrice()); //데이터를 수정한다.
-         }
+    }
 
 }
